9556baad91fb89027c86b596822afdc44903d39d
Vishnu Vardhan Reddy <gvvr2024@gmail.com>
2025-05-17 19:33:00 +0530

    health

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app.include_router(visitor.router, prefix="/api/visitor", tags=["Visitor"])
 @app.get("/api/health")
 async def health():
     """
-    Check status of server
+    Check status of backend server
     """
     return {"status":"Ok"}
 
